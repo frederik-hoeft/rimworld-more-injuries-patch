397ddb223809f41a233dd4a972c9d3ba32e293d7
frederik-hoeft <43813142+frederik-hoeft@users.noreply.github.com>
2023-08-10 22:28:59 +0200

    try to inject logging hook

--- Source/MoreInjuriesPatch/MoreInjuriesPatch/Patching/HarmonyPatches/ShockComp_CompTended_Patch.cs
+++ Source/MoreInjuriesPatch/MoreInjuriesPatch/Patching/HarmonyPatches/ShockComp_CompTended_Patch.cs
@@ -1,5 +1,4 @@
 ﻿using HarmonyLib;
-using Mono.Cecil.Cil;
 using MoreInjuriesPatch.Patching.TranspilerUtils;
 using Oof;
 using System.Collections.Generic;
@@ -18,10 +17,38 @@ public static class ShockComp_CompTended_Patch
         .GetProperty(nameof(ShockComp.BloodLoss))
         .GetGetMethod();
 
+    private static readonly MethodInfo _loggingHook = typeof(ShockComp_CompTended_Patch)
+        .GetMethod(nameof(OnFixedNow_LoggingHook), BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.DeclaredOnly);
+
+    /*
+     * ORIGINAL IL LOGIC:
+     * public override void CompTended(float quality, float maxQuality, int batchPosition = 0)
+     * {
+     *     base.CompTended(quality, maxQuality, batchPosition);
+     *     if ((quality < this.Props.BleedSeverityCurve.Evaluate(this.parent.Severity)) == false)
+     *     {
+     *         this.fixedNow = true;
+     *     }
+     *     return;
+     * }
+     * 
+     * PATCHED IL LOGIC:
+     * public override void CompTended(float quality, float maxQuality, int batchPosition = 0)
+     * {
+     *     base.CompTended(quality, maxQuality, batchPosition);
+     *     if (this.BloodLoss == null || (quality < this.Props.BleedSeverityCurve.Evaluate(this.parent.Severity)) == false)
+     *     {
+     *         this.fixedNow = true;
+     *         ShockComp_CompTended_Patch.OnFixedNow_LoggingHook(this, quality);
+     *     }
+     *     return;
+     * }
+     */
     static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
     {
         const string CHECKPOINT_1_NULL_CHECK = $"{nameof(ShockComp_CompTended_Patch)} transpiler checkpoint 1: null check injected";
         const string CHECKPOINT_2_IF_CONDITION_EXPANDED = $"{nameof(ShockComp_CompTended_Patch)} transpiler checkpoint 2: if-condition expanded";
+        const string CHECKPOINT_3_INSTALLED_LOGGING_HOOK = $"{nameof(ShockComp_CompTended_Patch)} transpiler checkpoint 3: logging hook installed!";
 
         const string TARGET_NAME = $"{nameof(ShockComp)}.{nameof(ShockComp.CompTended)}()";
 
@@ -29,7 +56,8 @@ public static class ShockComp_CompTended_Patch
 
         TranspiledMethodBody transpiledMethodBody = TranspiledMethodBody.Empty()
             .DefineCheckpoint(CHECKPOINT_1_NULL_CHECK)
-            .DefineCheckpoint(CHECKPOINT_2_IF_CONDITION_EXPANDED);
+            .DefineCheckpoint(CHECKPOINT_2_IF_CONDITION_EXPANDED)
+            .DefineCheckpoint(CHECKPOINT_3_INSTALLED_LOGGING_HOOK);
 
         long offset = 0;
         foreach(CodeInstruction instruction in instructions)
@@ -37,23 +65,36 @@ public static class ShockComp_CompTended_Patch
             Logger.LogVerbose($"{instruction.opcode} {instruction.operand}");
             if (instruction.opcode == OpCodes.Brfalse_S)
             {
-                transpiledMethodBody.Append(OpCodes.And);
+                // OR our null check together with whatever is already on the stack
+                transpiledMethodBody.Append(OpCodes.Or);
                 if (!transpiledMethodBody.TryCompleteCheckpoint(CHECKPOINT_2_IF_CONDITION_EXPANDED))
                 {
                     goto FAILURE;
                 }
             }
             transpiledMethodBody.Append(instruction);
-            if (instruction.opcode == OpCodes.Call && instruction.operand is MethodInfo { DeclaringType.Name: nameof(HediffComp.CompTended) })
+            if (instruction.opcode == OpCodes.Call && instruction.operand is MethodInfo { Name: nameof(HediffComp.CompTended), DeclaringType.Name: nameof(HediffComp) })
             {
-                Logger.LogVerbose($"Found {OpCodes.Call} into base implementation at offset 0x{offset:x8}! Injecting patch...");
+                Logger.LogVerbose($"Found {OpCodes.Call} into base implementation of {TARGET_NAME} at offset 0x{offset:x8}! Applying patch...");
                 // inject patch
                 transpiledMethodBody
                     .Append(OpCodes.Ldarg_0)                        // load "this" onto stack
                     .Append(OpCodes.Call, _instanceGetBloodloss)    // invoke this.BloodLoss, leave result on stack
                     .Append(OpCodes.Ldnull)                         // load null onto stack
-                    .Append(OpCodes.Cgt_Un);                        // null check (void* > NULL), leave result on stack
+                    .Append(OpCodes.Ceq);                           // null check (void* == NULL), leave result on stack
                 if (!transpiledMethodBody.TryCompleteCheckpoint(CHECKPOINT_1_NULL_CHECK))
+                {
+                    goto FAILURE;
+                }
+            }
+            if (instruction.opcode == OpCodes.Stfld)
+            {
+                // install logging hook
+                transpiledMethodBody
+                    .Append(OpCodes.Ldarg_0)
+                    .Append(OpCodes.Ldarg_1)
+                    .Append(OpCodes.Call, _loggingHook);
+                if (!transpiledMethodBody.TryCompleteCheckpoint(CHECKPOINT_3_INSTALLED_LOGGING_HOOK))
                 {
                     goto FAILURE;
                 }
@@ -76,5 +117,25 @@ public static class ShockComp_CompTended_Patch
     FAILURE:
         Logger.Error($"Failed to transpile {TARGET_NAME}! Reverting changes...");
         return instructions;
+    }
+
+    private static void OnFixedNow_LoggingHook(ShockComp shockComp, float quality)
+    {
+        if (Settings.enableVerboseLogging)
+        {
+            double requiredQuality = shockComp.Props.BleedSeverityCurve.Evaluate(shockComp.parent.Severity);
+            if ((double)quality >= requiredQuality)
+            {
+                Logger.Error($"{nameof(OnFixedNow_LoggingHook)} fired due to default behavior! Tending quality ({quality}) was good enough (>= {requiredQuality}) and hypovolemic shock *should* be fixed now :)");
+            }
+            else if (shockComp.BloodLoss is null)
+            {
+                Logger.Error($"{nameof(OnFixedNow_LoggingHook)} fired due to {nameof(ShockComp_CompTended_Patch)} (bloodloss was fixed)! Hypovolemic shock *should* be fixed now :)");
+            }
+            else
+            {
+                Logger.Error($"{nameof(OnFixedNow_LoggingHook)} fired for no reason :C {nameof(ShockComp_CompTended_Patch)} seems to be broken!");
+            }
+        }
     }
 }
